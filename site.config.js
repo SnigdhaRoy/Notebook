763d5736b6727ca6db64025e8aede269ba067627
Snigdha Roy <snigdha@snigdharoy.net>
2020-11-18 11:12:58 -0500

    updated description

--- site.config.js
+++ site.config.js
@@ -7,7 +7,7 @@ export default {
     </a>,
       summary: "I like to understand things and explain them well to others — this is my commitment to self-assigned learning. I hope this is helpful.",
     },
-    description: "A blog created with Next.js and Tailwind.css",
+    description: "Snigdha's Notebook for explaining while learning.",
     siteUrl: "https://nextjs-starter-blog-demo.netlify.app/",
     language: "en-US",
     social: {
