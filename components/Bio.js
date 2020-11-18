dee62d5debbcf8342f4cd98948d6f5bc6952d0ef
Snigdha Roy <snigdha@snigdharoy.net>
2020-11-17 20:21:03 -0500

    update

--- components/Bio.js
+++ components/Bio.js
@@ -19,7 +19,7 @@ export default function Bio({ className }) {
         Hey, I'm <b className="font-semibold">{author.name}</b>{" "}
         {author.summary}{" "}
         <a href={`https://twitter.com/${social.twitter}`}>
-          Follow along on twitter!
+          Say hello on twitter!
         </a>
       </p>
     </div>
